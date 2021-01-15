26c7e64daa2be22e922dda0868d1cbbd8e698f5f
dabee-ee <movie2529@naver.com>
2021-01-15 14:37:54 +0900

    15,Jan,2021 Practice_Web Scrapping

--- python/nomad/web_scrapping_0115/indeed.py
+++ python/nomad/web_scrapping_0115/indeed.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 LIMIT = 50
 URL = f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?as_and=%EA%B0%9C%EB%B0%9C%EC%9E%90&limit={LIMIT}"
 
-def extract_indeed_pages():
+def get_last_page():
   result = requests.get(URL)
 
   soup = BeautifulSoup(result.text, "html.parser")
@@ -28,17 +28,28 @@ def extract_job(html):
     else:
       company = str(company.string)
     company = company.strip()
-    location = html.find("span", {"class":"location"}).string
-    return {'title':title, 'company':company, 'location':location}
+    location = html.find("div", {"class":"recJobLoc"})["data-rc-loc"]
+    job_id = html["data-jk"]
+    return {
+      'title':title, 
+      'company':company, 
+      'location':location, "Link":f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?as_and=%EA%B0%9C%EB%B0%9C%EC%9E%90&limit=50&vjk={job_id}"
+      }
 
-def extract_indeed_jobs(last_page):
+def extract_jobs(last_page):
   jobs = []
-  # for page in range(last_page):
-  result = requests.get(f"{URL}&start={0*LIMIT}")
-  soup = BeautifulSoup(result.text, "html.parser")
-  results = soup.find_all("div", {"class":"jobsearch-SerpJobCard"})
-  for result in results:
-    job = extract_job(result)
-    jobs.append(job)
+  for page in range(last_page):
+    print(f"Scrpping sindeed: page {page}")
+    result = requests.get(f"{URL}&start={page*LIMIT}")
+    soup = BeautifulSoup(result.text, "html.parser")
+    results = soup.find_all("div", {"class":"jobsearch-SerpJobCard"})
+    for result in results:
+        job = extract_job(result)
+        jobs.append(job)
   return jobs
 
+
+def get_jobs():
+  last_page = get_last_page()
+  jobs = extract_jobs(last_page)
+  return jobs
